2617cbcc249258ed000ecfafa2d8670aac300ef6
ARNAUD DAOUKARE <56733142+AZAZEL00@users.noreply.github.com>
2019-11-10 19:51:14 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,14 @@
+#Arnaud Dakouare 18A862FS
+class Client:        
+    def __init__(self, nom, prenom, init_solde=0):
+        """Déclaration des attributs"""
+        self.nom = nom
+        self.prenom = prenom
+        self.solde=init_solde
+        self.numeroCompte = 1000000
 
+    def retrait(self, montant):
+        if montant > self.solde:
+            return False
+        self.solde -= montant
+return True

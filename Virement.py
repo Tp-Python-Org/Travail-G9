56dc4ae04164ae7cf41387b06c0593bfa52dd542
lagnabadoufienefabrice <56310238+lagnabadoufienefabrice@users.noreply.github.com>
2019-11-10 19:59:32 +0100

    Create Virement.py

---
+++ Virement.py
@@ -0,0 +1,10 @@
+#Lagnaba Doufiene Fabrice 18B485FS   
+class Virement(Guichetieres):
+    def __init__(self,virementEpargné,virementCourant): 
+        self.virementEpargné=virementEpargné
+        self.virementCourant=virementCourant
+        return"Virement:virement epargné{0},virement courant{1},virement multiple{2}".format(self.virementEpargné,self.virementCourant,self.virementMultiple)
+        virement=virement("")
+        Guichetiers.__init__(self,virementEpargné,viermentCourant)
+        self.virementMultiple=virementMultiple
+ 

8433dfe0f01da8870a8c39f7330583b08dd74714
BOPABE <57106399+BOPABE@users.noreply.github.com>
2019-11-10 20:28:24 +0100

    Create Banque.py

---
+++ Banque.py
@@ -0,0 +1,10 @@
+#Bopabe Warmai 18B607FS
+class Banque:
+    def __init__(self,Banque,adresseBanque):
+        self.Banque=Banque
+        self.adresseBanque=adresseBanque
+        nomBanque=Banque("B.A.C",52-41)
+        return"Banque:nom {0},dresse{1}".format(self.nom,self.adresse)
+
+compte = {}
+

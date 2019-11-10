ec298102c9eed589877ff8af8a27ebf752eaa57a
zoutchibe <56928414+zoutchibe@users.noreply.github.com>
2019-11-10 19:36:15 +0100

    Update Guichetier.py

--- Guichetier.py
+++ Guichetier.py
@@ -1,1 +1,11 @@
 #ZOUTCHIBE PALLAI 18B486FS
+
+class Guichetieres(Banque):
+     def __init__(self,nomGuichetier,prenomGuichetier,poste):
+        self.nom= nomGuichetier
+        self.prenom= prenomGuichetier
+        self.poste= poste
+        return "Guichetier:nom{0},prenom{1},poste{2}".format(self.nom,self.prenom,self.poste)
+        guichetier=guichetier("Ousmanou","Amadou",12)
+        Banque.__init__(self,nomGuichetuier,prenomGuichetier)
+        self.poste= poste

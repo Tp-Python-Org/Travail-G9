0f60864a1d97b4a261fab87c603a0b9eca6ecc7a
fadelasad11 <56893716+fadelasad11@users.noreply.github.com>
2019-11-10 20:13:18 +0100

    Create OperationInernationnal.py

---
+++ OperationInernationnal.py
@@ -0,0 +1,9 @@
+#Adelphe Rilembaye 18B708FS        
+class OperationInternationale(Guichetieres):
+    def __init__(self,compasation,dette):
+        self.compasation=compasation
+        self.dette=dette
+        return"operationInternationale:compasation{0},dette{1}".format(self.copasation,self.dette)
+        OperationInternationale=OperationInternationale("")
+        Guichetieres.__init__(self,copasation,dette)
+        self.dette=dette

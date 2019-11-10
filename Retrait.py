8c7a01cf565e36e3bd8b5776851f218b1994d0ea
BADEMANA BIGUE CHARLES <56733309+BADMAN00@users.noreply.github.com>
2019-11-10 19:52:48 +0100

    Update Retrait.py

--- Retrait.py
+++ Retrait.py
@@ -1,1 +1,11 @@
 
+#Bademana Bigue Charles 18A047FS         
+class Retrait(Guichetieres):
+    def __init__(self,gestionRetrait,gestionRetraitchek): 
+        self.gestionRetrait=gestionRetrait
+        self.gestionRetraitchek=gestionRetraitchek-montant
+        return"Retrait:gestionRetrait{0},gestionRetraitchek{1}".format(self.gestionRetrait,self.gestionRetraitchek)
+        Retrait=Retrait("")
+        Guichetieres.__init__(self,gestionRetrait)
+        self.gestionRetraitchek=gestionRetraitchek
+  

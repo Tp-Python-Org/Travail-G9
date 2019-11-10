8c5636d217d1dbfc775aff9117d6afa934c9563e
AlfredZ37 <56769912+AlfredZ37@users.noreply.github.com>
2019-11-10 19:35:26 +0100

    Update Controleur.py

--- Controleur.py
+++ Controleur.py
@@ -1,1 +1,10 @@
-
+#Mouaromtog Djimhognan 18B371FS
+class Controleurs(Banque):
+    def __init__(self,nom,matricule):
+        self.nom=nom
+        self.matricule=matricule
+        return "controleur{0},matricule{1}".format(self.nom,self.matricule)
+        controleur=controleur("bravo zoulou",123-456)
+        Banque.__init__(self,nom)
+        #qui fait l'appel au constructeur de la classe mére
+        self.matricule=matricule

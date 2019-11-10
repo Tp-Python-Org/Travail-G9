af5f48f3fc19b552b1f8727fb8f92d1db3341669
adoumadjiherve <56321013+adoumadjiherve@users.noreply.github.com>
2019-11-10 19:43:56 +0100

    Update Gestionnaire.py

--- Gestionnaire.py
+++ Gestionnaire.py
@@ -1,1 +1,13 @@
 
+#Adoumadji Noudjalbaye Herve 18A880FS        
+class Gestionnaires(Banque):
+    def __init__(self,nom,phone,email):
+        self.nom= nom
+        self.tel= phone
+        self.email= email
+        return "gestionnaire{0},nimero telephone{1},compte email{2}".format(self.nom,self.tel,self.email)
+        gestionnaire=gestionnaire("Mamadou ali",+10000000000,m@email.com)
+        Banque.__init__(self,nom,email)
+        #qui fait l'appel au constructeur de la classe mére
+        self.tel=phone
+        

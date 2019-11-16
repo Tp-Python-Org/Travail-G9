8bf3e048b14c9afbc9a2e9d8cb7b40057870d04c
ARNAUD DAOUKARE <56733142+AZAZEL00@users.noreply.github.com>
2019-11-16 07:24:18 +0100

    Update package.py

--- package.py
+++ package.py
@@ -95,7 +95,7 @@ class OperationInternationale(Guichetieres):
     def __init__(self,compasation,dette):
         self.compasation=compasation
         self.dette=dette
-        return"operationInternationale:compasation{0},dette{1}".format(self.copasation,self.dette)
+        return"operationInternationale:compasation{0},dette{1}".format(self.compasation,self.dette)
         OperationInternationale=OperationInternationale("")
         Guichetieres.__init__(self,copasation,dette)
         self.dette=dette
